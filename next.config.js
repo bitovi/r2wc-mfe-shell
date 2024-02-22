5dd1e09b9da29619f1bb1b3cdf2a8540fee6b0c7
David Nicholas <Dnicholas@bitovi.com>
2024-02-22 15:48:20 -0800

    update deployment setup

--- next.config.js
+++ next.config.js
@@ -1,18 +1,40 @@
 const { NextFederationPlugin } = require("@module-federation/nextjs-mf");
+// /**
+//  * @param {boolean} isServer
+//  */
+// const remotes = (isServer) => {
+//   return {
+//     ...createRemote(isServer, {
+//       mfeName: "header",
+//       serverUrl: "http://header:3001",
+//       chunkUrl: "http://localhost:3001",
+//     }),
+//     ...createRemote(isServer, {
+//       mfeName: "services",
+//       serverUrl: "http://services:3002",
+//       chunkUrl: "http://localhost:3002",
+//     }),
+//   };
+// };
+
 /**
  * @param {boolean} isServer
  */
 const remotes = (isServer) => {
   return {
     ...createRemote(isServer, {
-      mfeName: "header",
-      serverUrl: "http://header:3001",
-      chunkUrl: "http://localhost:3001",
+      mfeName: "services",
+      serverUrl:
+        "http://bitovi-r2wc-mfe-services-main-540863391.us-east-1.elb.amazonaws.com",
+      chunkUrl:
+        "http://bitovi-r2wc-mfe-services-main-540863391.us-east-1.elb.amazonaws.com",
     }),
     ...createRemote(isServer, {
-      mfeName: "services",
-      serverUrl: "http://services:3002",
-      chunkUrl: "http://localhost:3002",
+      mfeName: "header",
+      serverUrl:
+        "http://bitovi-r2wc-mfe-header-main-46250868.us-east-1.elb.amazonaws.com",
+      chunkUrl:
+        "http://bitovi-r2wc-mfe-header-main-46250868.us-east-1.elb.amazonaws.com",
     }),
   };
 };
